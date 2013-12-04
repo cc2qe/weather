5db7aedaae9ff9438a5a112ae3aab4da06b3a06f
Colby Chiang <cc2qe@virginia.edu>
2013-12-04 18:24:23 -0500

    initial commit

---
+++ getWeather.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ZIP=22903
+
+curl --silent "http://xml.weather.yahoo.com/forecastrss?p="${ZIP}"&u=c" | grep -E '(<title>Conditions|Current Conditions:|C<BR)' | sed -e 's/Current Conditions://' -e 's/<br \/>//' -e 's/<b>//' -e 's/<\/b>//' -e 's/<BR \/>//' -e 's/<description>//' -e 's/<\/description>//' -e 's/<title>//' -e 's/<\/title>//' | awk '{ if ($1 != "") print }'
+
+echo ''
+
+curl --silent "http://xml.weather.yahoo.com/forecastrss?p="${ZIP}"&u=c" | grep 'Forecast:' -A 2 | sed -e 's/<br \/>//' -e 's/<b>//' -e 's/<\/b>//' | sed -e 's/<BR \/>//' | sed -e 's/<BR \/>//'
+
+CURRENT=`curl --silent "http://xml.weather.yahoo.com/forecastrss?p="${ZIP}"&u=c" | grep -E '(C<BR)' | sed 's/,.*//g'`
+ICON=`cat ~/code/weather/icon_key.txt | awk "{ if (\\$1==\"$CURRENT\") print }" | awk '{ print $2 }'`
+
+cp /Users/cc2qe/code/weather/yahoo_weather_icons/HD_Yahoo_Icons/$ICON /Users/cc2qe/code/weather/current.png
+
+# curl --silent "http://weather.yahoo.com/united-states/virginia/charlottesville-12767090/" | grep "current-weather" | sed "s/.*background\:url(\'\(.*\)\')\ no-repeat.*/\1/" | xargs curl --silent -o /tmp/weather.png
+
+# below is the old command, which no longer works
+#curl --silent "http://weather.yahoo.com/united-states/virginia/charlottesville-12767090/" | grep "forecast-icon" | sed "s/.*background\:url(\'\(.*\)\')\;\ _background.*/\1/" | xargs curl --silent -o /tmp/weather.png
