6a5d477a36401acd261b141a7340fa34ee0dc440
Colby Chiang <cc2qe@virginia.edu>
2013-12-10 10:33:50 -0500

    fixed tab/space issue with icon key, and awk FS value

--- getWeather.sh
+++ getWeather.sh
@@ -9,9 +9,9 @@ echo ''
 curl --silent "http://xml.weather.yahoo.com/forecastrss?p="${ZIP}"&u=c" | grep 'Forecast:' -A 2 | sed -e 's/<br \/>//' -e 's/<b>//' -e 's/<\/b>//' | sed -e 's/<BR \/>//' | sed -e 's/<BR \/>//'
 
 CURRENT=`curl --silent "http://xml.weather.yahoo.com/forecastrss?p="${ZIP}"&u=c" | grep -E '(C<BR)' | sed 's/,.*//g'`
-ICON=`cat ~/code/weather/icon_key.txt | awk "{ if (\\$1==\"$CURRENT\") print }" | awk '{ print $2 }'`
+ICON=`cat ./icon_key.txt | awk "{ if (\\$1==\"$CURRENT\") print }" FS="\t" | awk '{ print $2 }' FS="\t" `
 
-cp /Users/cc2qe/code/weather/yahoo_weather_icons/HD_Yahoo_Icons/$ICON /Users/cc2qe/code/weather/current.png
+cp ./yahoo_weather_icons/HD_Yahoo_Icons/$ICON ./current.png
 
 # curl --silent "http://weather.yahoo.com/united-states/virginia/charlottesville-12767090/" | grep "current-weather" | sed "s/.*background\:url(\'\(.*\)\')\ no-repeat.*/\1/" | xargs curl --silent -o /tmp/weather.png
 
