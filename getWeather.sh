48e60968d5524d1037f6a7c6bbca264c471dc07d
Colby Chiang <cc2qe@virginia.edu>
2013-12-11 12:59:23 -0500

    fixed file path issue

--- getWeather.sh
+++ getWeather.sh
@@ -9,9 +9,9 @@ echo ''
 curl --silent "http://xml.weather.yahoo.com/forecastrss?p="${ZIP}"&u=c" | grep 'Forecast:' -A 2 | sed -e 's/<br \/>//' -e 's/<b>//' -e 's/<\/b>//' | sed -e 's/<BR \/>//' | sed -e 's/<BR \/>//'
 
 CURRENT=`curl --silent "http://xml.weather.yahoo.com/forecastrss?p="${ZIP}"&u=c" | grep -E '(C<BR)' | sed 's/,.*//g'`
-ICON=`cat ./icon_key.txt | awk "{ if (\\$1==\"$CURRENT\") print }" FS="\t" | awk '{ print $2 }' FS="\t" `
+ICON=`cat ~/code/weather/icon_key.txt | awk "{ if (\\$1==\"$CURRENT\") print }" FS="\t" | awk '{ print $2 }' FS="\t" `
 
-cp ./yahoo_weather_icons/HD_Yahoo_Icons/$ICON ./current.png
+cp ~/code/weather/yahoo_weather_icons/HD_Yahoo_Icons/$ICON ~/code/weather/current.png
 
 # curl --silent "http://weather.yahoo.com/united-states/virginia/charlottesville-12767090/" | grep "current-weather" | sed "s/.*background\:url(\'\(.*\)\')\ no-repeat.*/\1/" | xargs curl --silent -o /tmp/weather.png
 
